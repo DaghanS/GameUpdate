62c16d391565dc8bb03945bff43fc7a7193d1e58
Dağhan Sever <daghansever@gmail.com>
2022-10-05 00:18:42 +0300

    ItemSlot improved. Loadout selection started.

--- Gear/GearChooser.cs
+++ Gear/GearChooser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class GearChooser : MonoBehaviour, IPointerDownHandler
+public class GearChooser : MonoBehaviour, IPointerDownHandler, IDropHandler
 // click on one gear icon to get gear recommendations
 {
     public void OnPointerDown(PointerEventData eventData)
@@ -22,5 +22,25 @@ public class GearChooser : MonoBehaviour, IPointerDownHandler
         // on dragbegin: clone the gear at the same hierarchy, decrease transparency of current gear.
         // on drag: move
         // on dragend: if not in a correct place destroy. if is in a slot increase transparency and trigger slot fitting.
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        Transform drag = eventData.pointerDrag.transform;
+        if (eventData.pointerDrag != null)
+        {
+            if (this.transform.childCount == 0)
+            {
+                drag.SetParent(this.transform); // if slots empty, you can put it there.
+                drag.position = GetComponent<Transform>().position;
+            }
+            else
+            {
+                GameObject oldChild = this.transform.GetChild(0).gameObject;
+                Destroy(oldChild);
+                Transform dragCopy = Instantiate(drag, this.transform);
+                dragCopy.position = this.transform.position;
+            }
+        }
     }
 }
--- Gear/ItemSlot.cs
+++ Gear/ItemSlot.cs
@@ -15,14 +15,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour, IDropHandler
-{
+public class ItemSlot : MonoBehaviour, IDropHandler {
     public void OnDrop(PointerEventData eventData) {
         Debug.Log("OnDrop");
+        Transform drag = eventData.pointerDrag.transform;
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<Transform>().position = GetComponent<Transform>().position;
+            if (this.transform.childCount == 0)
+            {
+                drag.SetParent(this.transform); // if slots empty, you can put it there.
+                drag.position = GetComponent<Transform>().position;
+            }
+            else
+            {
+                Transform prevParent = drag.parent;
+                this.transform.GetChild(0).position = prevParent.position;
+                this.transform.GetChild(0).SetParent(prevParent);
+                drag.SetParent(this.transform);
+                drag.position = this.transform.position;
+            }
         }
     }
-
 }
